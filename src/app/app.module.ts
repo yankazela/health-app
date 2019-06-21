ae3a91dffa8b44898f8fc8c5fc2059102cf50d46
Yannick Kazadi <yannick.kazadi@entelect.co.za>
2019-06-21 09:08:20 +0200

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
 import { ControlFormService } from './control-form.service';
 import { HttpRequestService } from './http-request.service'
 import { CustExtBrowserXhr } from './cust-ext-browser-xhr';
@@ -14,7 +13,6 @@ import { BrowserXhr } from '@angular/http';
 import { APP_ROUTES } from './app.routes';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ForumLandingPageComponent } from './forum-landing-page/forum-landing-page.component';
 import { DoctorsDetailsComponent } from './doctors-details/doctors-details.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component'
@@ -22,10 +20,8 @@ import { ProfileComponent } from './profile/profile.component'
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
     LoginComponent,
     DashboardComponent,
-    ForumLandingPageComponent,
     DoctorsDetailsComponent,
     LogoutComponent,
     ProfileComponent
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
-import { SignupComponent } from './signup/signup.component'
 import { Routes } from '@angular/router'
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component'
-import { ForumLandingPageComponent } from './forum-landing-page/forum-landing-page.component'
 import { DoctorsDetailsComponent } from './doctors-details/doctors-details.component'
 import { LogoutComponent } from './logout/logout.component'
 import { ProfileComponent } from './profile/profile.component' 
 
 export const APP_ROUTES: Routes = [
-    {path: 'signup', component: SignupComponent},
     {path: 'login', component: LoginComponent},
     {path: 'logout', component: LogoutComponent},
     {path: 'dashboard', component: DashboardComponent},
-    {path: 'dashboard/league/:leaguename', component: ForumLandingPageComponent},
     {path: 'healthpro', component: DoctorsDetailsComponent},
     {path: 'editprofile', component: ProfileComponent}
 ]
